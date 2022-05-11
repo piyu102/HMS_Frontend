8e55468259a3825ff0162cc6b5d064687a373e29
piyu102 <50202721+piyu102@users.noreply.github.com>
2022-05-11 12:29:30 +0530

    Added Sentry for react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://7da0347e3318461f88e55ab9f362d788@o1240921.ingest.sentry.io/6394266",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 
 ReactDOM.render(
@@ -13,7 +25,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
