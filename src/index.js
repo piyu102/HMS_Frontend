b20c476672f9525f44586bf022426e9652f150c2
piyu102 <50202721+piyu102@users.noreply.github.com>
2022-05-11 14:53:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
-Sentry.init({
-  dsn: "https://7da0347e3318461f88e55ab9f362d788@o1240921.ingest.sentry.io/6394266",
-  integrations: [new BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 
 ReactDOM.render(
